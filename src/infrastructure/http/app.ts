93ace2826648b0043acf258181cd355916387d06
loj <samson.amdebirhan1@vodafone.com>
2021-01-24 02:50:53 +0100

    App Skeleton - 3 + added basic middlewares

--- src/infrastructure/http/app.ts
+++ src/infrastructure/http/app.ts
@@ -5,26 +5,37 @@ import config from '../../infrastructure/config';
 //var logger = require('../../infrastructure/logging/winston');
 import { v1Router } from './api/v1';
 
-const app = express();
+var createError = require('http-errors');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const path = require('path');
 
+
+const app = express();
 ////app.use(require('express-status-monitor')());
 ////app.use(require("morgan")("combined", { "stream": logger.stream }));
-
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 //app.use(app.router); 
-
 ////app.use(cors(origin))
 ////app.use(compression())
 ////app.use(helmet())
 ////app.use(morgan('combined'))
 //app.use('/api/v1', v1Router)
-
 app.use('/api/v1', v1Router)
 
-const path = require('path');
-app.set("view engine", "pug");
-app.set("views", path.join(__dirname, "views"));
+//var indexRouter = require('./routes/index');
+//var usersRouter = require('./routes/users');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+//app.set("view engine", "pug");
 
 
 const port = config.port;
@@ -42,3 +53,31 @@ app.listen(port, () => {
 v1Router.use('/users', usersRoutes);*/
 
 
+
+
+
+
+
+
+
+
+//app.use('/', indexRouter);
+//app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+/*app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;*/
